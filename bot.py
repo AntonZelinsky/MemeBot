23a06b47f9ad1be6e63878151ed1dd7ba34a913b
pozarnik <pozarnik@tut.by>
2023-01-26 17:42:24 +0300

    delete comments and instruction

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from environs import Env
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -8,36 +9,22 @@ from telegram.ext import (
     MessageHandler
 )
 
-# Номер твоего профиля, используется для ограничения постинга только с твоего ака
-MY_ID = None
-# Номер группы в которую надо постить, начинается со знака '-' (число)
-GROUP_ID = None
-# Описание к публикуемым фото/видео и тд
-DESCRIBE = ""
-# Токен бота
-BOT_TOKEN = ""
+env = Env()
+env.read_env()
+
+MY_ID = env("MY_ID")
+GROUP_ID = env("GROUP_ID")
+DESCRIBE = env("DESCRIBE")
+BOT_TOKEN = env("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Выводит стартовое приветствие."""
-    text = """
-    Иструкция для бота:
-    1) Запишите в переменную MY_ID номер вашего чата (в сообщении ниже)
-    2) Заполните переменную DESCRIBE, она содержит подпись к вашим сообщениям.
-    3) Добавьте бота в вашу группу, разрешив публиковать посты
-    4) Заполните переменную GROUP_ID номером чата группы (придет в чат бота после добавления бота в группу)
-    5) Удалите бота из группы, перезапустите бота с заполненными данными, перезагрузте бота, добавьте бота в нужную группу
-    """
-    # Печатает стартовое приветствие
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-    # Печатает id твоего профиля
+    """Печатает id твоего профиля при запуске бота."""
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.effective_chat.id)
 
 
 async def track_chats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """В режиме реального времени выводит название и номер группы, в которую добавляют бота с правами админа."""
-    # Проверяет статус чата бота в режиме online, если появляется новый чат со статусом administrator,
-    # в который ТЫ его добавил, то печатает id и название этого чата (такие чаты только в группе)
     if update.my_chat_member.new_chat_member.status == 'administrator':
         text = f"Группа '{update.my_chat_member.chat.title}' ID: {update.my_chat_member.chat.id}"
         await context.bot.send_message(chat_id=update.my_chat_member.from_user.id, text=text)
@@ -45,18 +32,14 @@ async def track_chats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
 async def forward_to_your_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Публикует фото, анимацию, аудио и документы в группу из переменной group_chat."""
-    # Проверяет заполнены ли данные
     if MY_ID is None or GROUP_ID is None:
         text = "Заполните переменные MY_ID и GROUP_ID"
         await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
         return
-    # Та самая проверка на ID профиля, чтобы посты от других не постились в твою группу
     if MY_ID != update.effective_chat.id:
         text = "Ваш id не соответствует MY_ID"
         await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
         return
-    # Проверка на тип входящего сообщения к боту: фото/анимация/аудио/документ/видео и
-    # отправка этого вложения в чат группы GROUP_ID с подписью DESCRIBE
     if update.message.photo:
         await context.bot.send_photo(
             chat_id=GROUP_ID,
@@ -91,12 +74,7 @@ async def forward_to_your_group(update: Update, context: ContextTypes.DEFAULT_TY
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    # Стартовое приветствие с инструкцией и выводом твоего id, после заполнения переменных можно отключить/удалить
     application.add_handler(CommandHandler("start", start))
-    # Выводит в реальном времени информацию группы, в которую ТЫ добавил бота, после заполнения данных можно
-    # отключить/удалить
     application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
-    # То самое что тебе надо, тупо проверяет тип пришедших боту данных, чтобы знать где взять id вложения и
-    # отправляет в нужную тебе группу сообщение с этим вложением и подписью из переменной DESCRIBE
     application.add_handler(MessageHandler(filters.ALL, forward_to_your_group))
     application.run_polling()
