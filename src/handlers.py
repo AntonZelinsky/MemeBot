37e8167bf01ddbc79ad1fdab610733f0192fc05e
pozarnik <pozarnik@tut.by>
2023-02-04 00:50:07 +0300

    #7 added work with db, clean trash

--- src/handlers.py
+++ src/handlers.py
@@ -1,50 +1,83 @@
-from telegram import Update
+from telegram import Chat, ChatMember, Update
 from telegram.ext import ContextTypes
 
-from src.setting import DESCRIPTION, GROUP_ID, MY_ID
+from src import services
+from src.db import channel_crud, get_async_session, user_crud
+from src.settings import DESCRIPTION
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Печатает id твоего профиля при запуске бота."""
-    text = f"Ваш ID: {update.effective_chat.id}, добавьте его в переменную MY_ID"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+async def save_or_update_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Добавляем либо обновляем профиль пользователя в БД."""
+    current_user_data = services.user_parser(update.effective_chat)
+    get_session = get_async_session()
+    session = await get_session.__anext__()
+    user_db = await user_crud.get_user(current_user_data["account_id"], session)
+    if user_db is None:
+        await user_crud.create(current_user_data, session)
+    else:
+        await user_crud.update(user_db, current_user_data, session)
+        await services.activate_user(account_id=current_user_data["account_id"], session=session)
 
 
 async def track_chats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """В режиме реального времени выводит название и номер группы, в которую добавляют бота с правами админа."""
-    if update.my_chat_member.new_chat_member.status == "administrator":
-        text = (
-            f"Группа '{update.my_chat_member.chat.title}' ID: {update.my_chat_member.chat.id}, "
-            f"добавьте его в переменную GROUP_ID"
-        )
-        await context.bot.send_message(chat_id=update.my_chat_member.from_user.id, text=text)
+    """В режиме реального времени доступ бота к чатам."""
+    chat = update.effective_chat
+    my_chat = update.my_chat_member
+    account_id = my_chat.from_user.id
+    get_session = get_async_session()
+    session = await get_session.__anext__()
+    # Отслеживаем блокировку чата пользователя и деактивируем его профиль
+    if chat.type in Chat.PRIVATE and my_chat.new_chat_member.status in ChatMember.BANNED:
+        await services.activate_user(account_id=account_id, session=session, status=False)
+    # Отслеживаем действия в чатах каналов и добавляем канал в базу, либо его деактивируем
+    if chat.type in Chat.CHANNEL:
+        if (
+            my_chat.new_chat_member.status in ChatMember.ADMINISTRATOR
+            and my_chat.old_chat_member.status not in ChatMember.ADMINISTRATOR
+        ):
+            user_db = await user_crud.get_user(account_id, session)
+            current_channel_data = services.channel_parser(my_chat.chat, user_db)
+            await channel_crud.create(current_channel_data, session)
+            text = f"Бот добавлен в канал '{my_chat.chat.title}'"
+            await context.bot.send_message(chat_id=account_id, text=text)
+
+        elif my_chat.new_chat_member.status in ChatMember.ADMINISTRATOR:
+            text = f"У бота в канале '{my_chat.chat.title}' изменены права"
+            await context.bot.send_message(chat_id=account_id, text=text)
+        # При удалении бота из канала деактивируем чат в базе
+        else:
+            await services.deactivate_channel(my_chat.chat.id, session)
+            text = f"Бот удален из канала '{my_chat.chat.title}'"
+            await context.bot.send_message(chat_id=account_id, text=text)
 
 
 async def forward_to_your_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Публикует фото, анимацию, аудио и документы в группу из переменной group_chat."""
-    if MY_ID is None or GROUP_ID is None:
-        text = "Заполните переменные MY_ID и GROUP_ID"
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-        return
-    if MY_ID != update.effective_chat.id:
-        text = "Ваш id не соответствует MY_ID"
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-        return
+    get_session = get_async_session()
+    session = await get_session.__anext__()
+    user_db = await user_crud.get_user(update.effective_user.id, session)
+    channel_db = await channel_crud.get_channel_by_user(user_db, session)
     if update.message.photo:
-        await context.bot.send_photo(chat_id=GROUP_ID, photo=update.message.photo[0].file_id, caption=DESCRIPTION)
+        await context.bot.send_photo(
+            chat_id=channel_db.channel_id, photo=update.message.photo[0].file_id, caption=DESCRIPTION
+        )
     elif update.message.animation:
         await context.bot.send_animation(
-            chat_id=GROUP_ID,
+            chat_id=channel_db.channel_id,
             animation=update.message.animation.file_id,
             caption=DESCRIPTION,
         )
     elif update.message.audio:
-        await context.bot.send_audio(chat_id=GROUP_ID, audio=update.message.audio.file_id, caption=DESCRIPTION)
+        await context.bot.send_audio(
+            chat_id=channel_db.channel_id, audio=update.message.audio.file_id, caption=DESCRIPTION
+        )
     elif update.message.document:
         await context.bot.send_document(
-            chat_id=GROUP_ID,
+            chat_id=channel_db.channel_id,
             document=update.message.document.file_id,
             caption=DESCRIPTION,
         )
     elif update.message.video:
-        await context.bot.send_video(chat_id=GROUP_ID, video=update.message.video.file_id, caption=DESCRIPTION)
+        await context.bot.send_video(
+            chat_id=channel_db.channel_id, video=update.message.video.file_id, caption=DESCRIPTION
+        )
