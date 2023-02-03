42f039389eec3571eef347f28b5f5a496477b111
pozarnik <pozarnik@tut.by>
2023-02-04 00:48:18 +0300

    #7 style fix

--- src/bot.py
+++ src/bot.py
@@ -6,13 +6,13 @@ from telegram.ext import (
     filters,
 )
 
-from src.handlers import forward_to_your_group, start, track_chats
-from src.setting import BOT_TOKEN
+from src.handlers import forward_to_your_group, save_or_update_user, track_chats
+from src.settings import BOT_TOKEN
 
 
 def create_bot():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", save_or_update_user))
     application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(MessageHandler(filters.ALL, forward_to_your_group))
     return application
