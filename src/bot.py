2d616da1d773cced23a6542fcb9f731a6399d1a5
pozarnik <pozarnik@users.noreply.github.com>
2023-02-06 21:34:06 +0300

    #7 delete start CommandHandler

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,11 @@
-from telegram.ext import (
-    ApplicationBuilder,
-    ChatMemberHandler,
-    CommandHandler,
-    MessageHandler,
-    filters,
-)
+from telegram.ext import ApplicationBuilder, ChatMemberHandler, MessageHandler, filters
 
-from src.handlers import forward_to_your_group, save_or_update_user, track_chats
+from src.handlers import forward_to_your_group, track_chats
 from src.settings import BOT_TOKEN
 
 
 def create_bot():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("start", save_or_update_user))
     application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(MessageHandler(filters.ALL, forward_to_your_group))
     return application
