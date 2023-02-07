285106cc540ef9d42753a8e9cc5bcf8d2cee6a04
pozarnik <pozarnik@users.noreply.github.com>
2023-02-08 00:23:28 +0300

    #7 added check_bot_privileges

--- src/services.py
+++ src/services.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from telegram import Chat, User
+from telegram import Chat, ChatMember, User
 
 from src.db import channel_crud, user_crud
-from src.models import User as user_model
 
 
 def user_parser(user: User) -> dict:
-    """Парсит данные пользователя из чата."""
+    """Парсит данные пользователя."""
     parsed_user = {
         "account_id": user.id,
         "first_name": user.first_name,
@@ -16,13 +15,13 @@ def user_parser(user: User) -> dict:
     return parsed_user
 
 
-def channel_parser(chat: Chat, user: user_model) -> dict:
-    """Парсит данные канала из чата."""
+def channel_parser(chat: Chat, user_id: int) -> dict:
+    """Парсит данные канала."""
     channel = {
         "channel_id": chat.id,
         "title": chat.title,
         "username": chat.username or None,
-        "invited_user_id": user.id,
+        "invited_user_id": user_id,
     }
     return channel
 
@@ -36,7 +35,14 @@ async def activate_user(user_id: int, session: AsyncSession, status: bool = True
 async def deactivate_channel(channel_id: int, session: AsyncSession):
     """Изменяет статус is_active у канала на False."""
     is_active = {"is_active": False}
-    channel_db = await channel_crud.get_channel(channel_id, session)
-    if channel_db:
-        await channel_crud.update(channel_db, is_active, session)
-        return channel_db
+    await channel_crud.update(channel_id, is_active, session)
+
+
+def check_bot_privileges(current_channel_chat: ChatMember) -> str:
+    """Проверяет у бота доступ к постингу сообщений в канале."""
+    text = "отправки сообщений в группу!"
+    if current_channel_chat.can_post_messages is True:
+        text = " есть права для " + text
+    else:
+        text = " отсутствуют права для " + text
+    return text
