af546e57471286581bad713702820102c0383678
pozarnik <pozarnik@users.noreply.github.com>
2023-02-10 04:58:05 +0300

    #7 added filters

--- src/bot.py
+++ src/bot.py
@@ -7,5 +7,9 @@ from src.settings import BOT_TOKEN
 def create_bot():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
-    application.add_handler(MessageHandler(filters.ChatType.PRIVATE & filters.ATTACHMENT, forward_attachment))
+    application.add_handler(
+        MessageHandler(
+            filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION), forward_attachment
+        )
+    )
     return application
