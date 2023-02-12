e0dd7a3263c8cf423740380f51a0f06a15c7144d
pozarnik <pozarnik@users.noreply.github.com>
2023-02-12 22:42:32 +0300

    #7 changed handlers name

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,15 @@
 from telegram.ext import ApplicationBuilder, ChatMemberHandler, MessageHandler, filters
 
-from src.handlers import forward_attachment, track_chats
+from src.handlers import forward_attachment_handler, track_chats_handler
 from src.settings import BOT_TOKEN
 
 
 def create_bot():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
+    application.add_handler(ChatMemberHandler(track_chats_handler, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(
         MessageHandler(
-            filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION), forward_attachment
+            filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION), forward_attachment_handler
         )
     )
     return application
