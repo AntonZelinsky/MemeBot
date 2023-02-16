95950f398b84f1d824080cce071639148eec75f3
pozarnik <pozarnik@users.noreply.github.com>
2023-02-16 10:33:22 +0300

    #7 changed create and update

--- src/db/crud.py
+++ src/db/crud.py
@@ -1,26 +1,31 @@
-from typing import Optional
+from typing import Optional, TypeVar
 
-from sqlalchemy import insert, select, update
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from src.db.models import Channel, User
 from src.settings import DATABASE_URL
+
+DatabaseModel = TypeVar("DatabaseModel")
 
 
 class BaseCRUD:
     def __init__(self, model):
         self._model = model
 
-    async def create(self, new_data: dict, session: AsyncSession):
-        """Создает объект текущей модели и возвращает его id."""
-        obj_id = await session.execute(insert(self._model).values(new_data).returning(self._model.id))
+    async def create(self, new_data: DatabaseModel, session: AsyncSession):
+        """Создает объект текущей модели и возвращает его."""
+        session.add(new_data)
         await session.commit()
-        return obj_id.scalars().first()
+        await session.refresh(new_data)
+        return new_data
 
-    async def update(self, object_id: int, update_data: dict, session: AsyncSession):
-        """Обновляет объект текущей модели."""
-        await session.execute(update(self._model).where(self._model.id == object_id).values(update_data))
+    async def update(self, object_id: int, update_data: DatabaseModel, session: AsyncSession) -> DatabaseModel:
+        """Обновляет объект текущей модели и возвращает его."""
+        update_data.id = object_id
+        update_data = await session.merge(update_data)
         await session.commit()
+        return update_data
 
 
 class UserCRUD(BaseCRUD):
@@ -31,7 +36,7 @@ class UserCRUD(BaseCRUD):
 
 
 class ChannelCRUD(BaseCRUD):
-    async def get_channel(self, channel_id: int, session: AsyncSession) -> Optional[User]:
+    async def get_channel(self, channel_id: int, session: AsyncSession) -> Optional[Channel]:
         """Возвращает объект Channel из БД по его channel_id."""
         channel = await session.execute(
             select(self._model).where(self._model.channel_id == channel_id).where(self._model.is_active == True)
