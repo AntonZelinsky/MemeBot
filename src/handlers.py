282afb98702df3c4ae0cd8b66c63aca609d84c2c
pozarnik <pozarnik@users.noreply.github.com>
2023-02-24 17:03:53 +0300

    #7 replace context -> context.bot

--- src/handlers.py
+++ src/handlers.py
@@ -15,7 +15,7 @@ async def track_chats_handler(update: Update, context: ContextTypes.DEFAULT_TYPE
         await services.check_private_chat_status(effective_user, current_status)
 
     if my_chat.chat.type in Chat.CHANNEL:
-        await services.check_channel_chat(effective_user, my_chat, current_status, previous_status, context)
+        await services.check_channel_chat(effective_user, my_chat, current_status, previous_status, context.bot)
 
 
 async def forward_attachment_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -24,4 +24,4 @@ async def forward_attachment_handler(update: Update, context: ContextTypes.DEFAU
     for channel in user_db.channels:
         if channel and channel.is_active is True:
             channel_id = channel.channel_id
-            await services.posting_message(update.message, channel_id, context)
+            await services.posting_message(update.message, channel_id, context.bot)
