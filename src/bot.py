01d3c3b92d0acdef9323b28b3ff4cc873be34b46
pozarnik <pozarnik@users.noreply.github.com>
2023-03-02 14:37:16 +0300

    #7 added start handler

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,23 @@
-from telegram.ext import ApplicationBuilder, ChatMemberHandler, MessageHandler, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    ChatMemberHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 
-from src.handlers import forward_attachment_handler, track_chats_handler
+from src.handlers import forward_attachment_handler, start, track_chats_handler
 from src.settings import BOT_TOKEN
 
 
 def create_bot():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
     application.add_handler(ChatMemberHandler(track_chats_handler, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(
         MessageHandler(
-            filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION), forward_attachment_handler
-        )
+            filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION),
+            forward_attachment_handler,
+        ),
     )
     return application
