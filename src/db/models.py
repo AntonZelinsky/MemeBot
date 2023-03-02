d6581b5b4f260e656189615e0987866e1b36b376
pozarnik <pozarnik@users.noreply.github.com>
2023-03-02 14:50:55 +0300

    #7 added from_parse classmethod in models

--- src/db/models.py
+++ src/db/models.py
@@ -22,19 +22,32 @@ class User(Base):
     last_name: Mapped[Optional[str]]
     username: Mapped[Optional[str]]
     channels: Mapped[List["Channel"]] = relationship(
-        back_populates="user", lazy="selectin", cascade="all, delete-orphan"
+        back_populates="user",
+        lazy="selectin",
+        cascade="all, delete-orphan",
     )
     is_active: Mapped[bool] = mapped_column(default=True)
 
+    @classmethod
+    def from_parse(cls, chat_data) -> "User":
+        """Парсит данные из data в модель User."""
+        return cls(
+            account_id=chat_data.id,
+            first_name=chat_data.first_name,
+            last_name=chat_data.last_name or None,
+            username=chat_data.username or None,
+            is_active=True,
+        )
+
     def __repr__(self) -> str:
         return (
-            f"User: "
+            f"User("
             f"id={self.id!r}, "
             f"account_id={self.account_id!r}, "
             f"first_name={self.first_name!r}, "
             f"last_name={self.last_name!r}, "
             f"username={self.username!r}, "
-            f"is_active={self.is_active!r}"
+            f"is_active={self.is_active!r})"
         )
 
 
@@ -49,12 +62,22 @@ class Channel(Base):
     user: Mapped["User"] = relationship(back_populates="channels", lazy="selectin")
     is_active: Mapped[bool] = mapped_column(default=True)
 
+    @classmethod
+    def from_parse(cls, chat_data, user_id: int) -> "Channel":
+        """Парсит данные из data в модель User."""
+        return cls(
+            channel_id=chat_data.id,
+            title=chat_data.title,
+            username=chat_data.username or None,
+            invited_user_id=user_id,
+        )
+
     def __repr__(self) -> str:
         return (
-            f"Channel: "
+            f"Channel("
             f"id={self.id!r}, "
             f"channel_id={self.channel_id!r}, "
             f"title={self.title!r}, "
             f"invited_user_id={self.invited_user_id!r}, "
-            f"is_active={self.is_active!r}"
+            f"is_active={self.is_active!r})"
         )
