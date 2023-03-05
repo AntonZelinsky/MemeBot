66fdb4c14261515464acc2ec6c7d09745152d31b
pozarnik <pozarnik@users.noreply.github.com>
2023-03-05 14:41:29 +0300

    #7 added generic, rename to repository

--- src/db/base.py
+++ src/db/base.py
@@ -1,3 +1,5 @@
+from typing import Generic, TypeVar
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
@@ -7,15 +9,17 @@ from src.settings import DATABASE_URL
 engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
+T = TypeVar("T")
 
-class BaseManager(object):
+
+class BaseRepository(Generic[T]):
     """Создает и обновляет объект в БД."""
 
-    def __init__(self, model, session: AsyncSession) -> None:
+    def __init__(self, model: T, session: AsyncSession) -> None:
         self._model = model
         self._session = session
 
-    async def create(self, new_data):
+    async def create(self, new_data: T) -> T:
         """Создает объект текущей модели и возвращает его."""
         self._session.add(new_data)
         await self._session.commit()
@@ -23,7 +27,7 @@ class BaseManager(object):
         await self._session.close()
         return new_data
 
-    async def update(self, object_id: int, update_data):
+    async def update(self, object_id: int, update_data: T) -> T:
         """Обновляет объект текущей модели и возвращает его."""
         update_data.id = object_id
         update_data = await self._session.merge(update_data)
@@ -32,33 +36,33 @@ class BaseManager(object):
         return update_data
 
 
-class UserManager(BaseManager):
+class UserRepository(BaseRepository[User]):
     """Класс для работы с моделью User в БД."""
 
     def __init__(self) -> None:
         super().__init__(User, async_session())
 
-    async def get_user(self, account_id: int) -> User | None:
+    async def get_user(self, account_id: int) -> User:
         """Возвращает объект User из БД по его account_id."""
-        user = await self._session.scalar(select(self._model).where(self._model.account_id == account_id))
+        user = await self._session.scalars(select(self._model).where(self._model.account_id == account_id))
         await self._session.close()
-        return user
+        return user.one()
 
 
-class ChannelManager(BaseManager):
+class ChannelRepository(BaseRepository[Channel]):
     """Класс для работы с моделью Channel в БД."""
 
     def __init__(self) -> None:
         super().__init__(Channel, async_session())
 
-    async def get_channel(self, channel_id: int) -> Channel | None:
+    async def get_channel(self, channel_id: int) -> Channel:
         """Возвращает объект Channel из БД по его channel_id."""
-        channel = await self._session.scalar(
+        channel = await self._session.scalars(
             select(self._model).where(self._model.channel_id == channel_id).where(self._model.is_active),
         )
         await self._session.close()
-        return channel
+        return channel.one()
 
 
-user_manager = UserManager()
-channel_manager = ChannelManager()
+user_manager = UserRepository()
+channel_manager = ChannelRepository()
