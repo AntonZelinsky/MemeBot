c85072430edb604cce93d2f7b35b280038de46cf
pozarnik <pozarnik@users.noreply.github.com>
2023-03-07 23:06:36 +0300

    #10 added type_channel_for_binding and bind_the_channel, changed description message

--- src/menu_commands.py
+++ src/menu_commands.py
@@ -9,8 +9,19 @@ x = [
 ]
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+async def start_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    # start_text = (
+    #     "Привет. Я — Мем бот и буду помогать вам постить мемы на ваши каналы.\n"
+    #     "Чтобы начать работу, нужно добавить каналы на которые вы хотите  постить сообщения.\n"
+    #     "Для того чтобы связать ваш канал с вашей учётной записью, добавьте этот бот в администраторы канала и "
+    #     "перешлите с канала сообщение сюда. Если и вы и бот являетесь администраторами канала, то будет создана "
+    #     "связь и вы сможете постить сообщения туда. Боту на канале достаточно дать права на отправку сообщений."
+    # )
+    # print(f"menu up={update}")
     buttons = [
+        [
+            InlineKeyboardButton("Связать новый канал", callback_data=callback_data.CALLBACK_BIND_THE_CHANNEL_COMMAND),
+        ],
         [
             InlineKeyboardButton("Подключенные каналы", callback_data=callback_data.CALLBACK_MY_CHANNELS_COMMAND),
         ],
@@ -23,9 +34,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
         await update.callback_query.answer()
         await update.callback_query.edit_message_text(text="Стартовое меню", reply_markup=keyboard)
     else:
+        # await update.message.reply_text(start_text)
         await update.message.reply_text("Стартовое меню", reply_markup=keyboard)
 
-    return states.START_STATE
+    return states.MAIN_MENU_STATE
+
+
+async def bind_the_channel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    text = "Для привязки канала в котором уже есть бот введите сокрещение канала в виде @channel."
+
+    await update.callback_query.answer()
+    await update.callback_query.edit_message_text(text=text)
+    return states.TYPING_BINDING_CHANNEL
+
+
+async def type_channel_for_binding(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    user_data = context.user_data
+    user = update.effective_user.id
+    user_data[user] = update.message.text
+    await update.message.reply_text(update.message.text)
+    return await start_menu(update, context)
 
 
 async def my_channels(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
@@ -34,7 +62,7 @@ async def my_channels(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str
     channels_buttons = []
     for i in x:
         channels_buttons.append(
-            [InlineKeyboardButton(i["название"], callback_data=callback_data.CALLBACK_CHANNEL_MENU_COMMAND)]
+            [InlineKeyboardButton(i["название"], callback_data=callback_data.CALLBACK_CHANNEL_MENU_COMMAND)],
         )
     back_button = [[InlineKeyboardButton("Назад", callback_data=callback_data.CALLBACK_BACK_TO_MAIN_COMMAND)]]
     buttons = channels_buttons + back_button
@@ -72,18 +100,19 @@ async def ask_for_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> s
 
     await update.callback_query.answer()
     await update.callback_query.edit_message_text(text=text)
-    return states.TYPING_STATE
+    return states.TYPING_DESCRIPTION
 
 
 async def edit_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     user_data = context.user_data
     user = update.effective_user.id
     user_data[user] = update.message.text
+    await update.message.reply_text(update.message.text)
     return await channel_menu(update, context)
 
 
 async def close_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
-    await query.edit_message_text(text="Открыть меню можно командой /start")
+    await query.edit_message_text(text="Открыть меню можно командой /menu")
     return ConversationHandler.END
