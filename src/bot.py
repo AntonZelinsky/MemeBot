5192a35f3558fcf82f72a50a7ac636a4cfa7f7e7
pozarnik <pozarnik@users.noreply.github.com>
2023-03-07 23:03:00 +0300

    #10 changed import and menu_handler

--- src/bot.py
+++ src/bot.py
@@ -8,25 +8,29 @@ from telegram.ext import (
     filters,
 )
 
-from src import menu_commands
+from src import handlers, menu_commands, settings
 from src.constants import callback_data, states
-from src.handlers import forward_attachment_handler, track_chats_handler
-from src.settings import BOT_TOKEN
 
 
 def create_bot():
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application = ApplicationBuilder().token(settings.BOT_TOKEN).build()
     menu_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", menu_commands.start)],
+        entry_points=[CommandHandler("menu", menu_commands.start_menu, filters.ChatType.PRIVATE)],
         states={
-            states.START_STATE: [CallbackQueryHandler(menu_commands.start, pattern="start")],
+            states.START_MENU_STATE: [
+                CallbackQueryHandler(menu_commands.start_menu, pattern=callback_data.CALLBACK_START_MENU_COMMAND),
+            ],
             states.MAIN_MENU_STATE: [
+                CallbackQueryHandler(
+                    menu_commands.bind_the_channel,
+                    pattern=callback_data.CALLBACK_BIND_THE_CHANNEL_COMMAND,
+                ),
                 CallbackQueryHandler(menu_commands.my_channels, pattern=callback_data.CALLBACK_MY_CHANNELS_COMMAND),
                 CallbackQueryHandler(menu_commands.close_menu, pattern=callback_data.CALLBACK_CLOSE_MENU_COMMAND),
             ],
             states.MY_CHANNELS_STATE: [
                 CallbackQueryHandler(menu_commands.channel_menu, pattern=callback_data.CALLBACK_CHANNEL_MENU_COMMAND),
-                CallbackQueryHandler(menu_commands.start, pattern=callback_data.CALLBACK_BACK_TO_MAIN_COMMAND),
+                CallbackQueryHandler(menu_commands.start_menu, pattern=callback_data.CALLBACK_BACK_TO_MAIN_COMMAND),
             ],
             states.CHANNEL_MENU_STATE: [
                 CallbackQueryHandler(
@@ -42,19 +46,24 @@ def create_bot():
                     pattern=callback_data.CALLBACK_BACK_TO_CHANNELS_COMMAND,
                 ),
             ],
-            states.TYPING_STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_commands.edit_description)],
+            states.TYPING_DESCRIPTION: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, menu_commands.edit_description),
+            ],
+            states.TYPING_BINDING_CHANNEL: [
+                MessageHandler(filters.TEXT, menu_commands.type_channel_for_binding),
+            ],
         },
         fallbacks=[
-            CommandHandler("start", menu_commands.start),
+            CommandHandler("menu", menu_commands.start_menu),
         ],
     )
-
+    application.add_handler(CommandHandler("start", handlers.start, filters.ChatType.PRIVATE))
     application.add_handler(menu_handler)
-    application.add_handler(ChatMemberHandler(track_chats_handler, ChatMemberHandler.MY_CHAT_MEMBER))
+    application.add_handler(ChatMemberHandler(handlers.track_chats_handler, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(
         MessageHandler(
             filters.ChatType.PRIVATE & (filters.VIDEO | filters.PHOTO | filters.ANIMATION),
-            forward_attachment_handler,
+            handlers.forward_attachment_handler,
         ),
     )
     return application
