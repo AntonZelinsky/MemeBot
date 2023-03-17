2edcac2af4b10df5d38405de985e4c72bfe63590
pozarnik <pozarnik@users.noreply.github.com>
2023-03-17 23:43:28 +0300

    #10 added exceptions, refactoring

--- src/menu_commands.py
+++ src/menu_commands.py
@@ -1,25 +1,25 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes, ConversationHandler
 
-from src import services
+from src import exceptions, services
 from src.constants import callback_data, constants, states
 from src.db import base
 
 
-async def start_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Стартовое меню."""
+async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Выводит главное меню при вызове команды /menu."""
+    try:
+        await base.user_repository.get(update.effective_user.id)
+    except exceptions.UserNotFoundError:
+        await update.message.reply_text(text="Вы не зарегистрированы. Для регистрации введите команду /register")
+        raise
+
     context.user_data[constants.STOP_FORWARD] = False
-    register_button = [[InlineKeyboardButton("Зарегистрироваться", callback_data=callback_data.CALLBACK_REGISTER)]]
-    bind_button = [[InlineKeyboardButton("Привязать канал", callback_data=callback_data.CALLBACK_BIND_THE_CHANNEL)]]
-    channels_button = [[InlineKeyboardButton("Подключенные каналы", callback_data=callback_data.CALLBACK_MY_CHANNELS)]]
-    buttons = [[InlineKeyboardButton("Закрыть меню", callback_data=callback_data.CALLBACK_CLOSE_MENU)]]
-
-    user = await base.user_repository.get_user(update.effective_user.id)
-
-    if not user:
-        buttons = register_button + buttons
-    else:
-        buttons = bind_button + channels_button + buttons
+    buttons = [
+        [InlineKeyboardButton("Привязать канал", callback_data=callback_data.CALLBACK_CALL_BINDING)],
+        [InlineKeyboardButton("Подключенные каналы", callback_data=callback_data.CALLBACK_USER_CHANNELS)],
+        [InlineKeyboardButton("Закрыть меню", callback_data=callback_data.CALLBACK_CLOSE_MENU)],
+    ]
 
     keyboard = InlineKeyboardMarkup(buttons)
 
@@ -28,44 +28,44 @@ async def start_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
         await update.callback_query.edit_message_reply_markup(reply_markup=keyboard)
     else:
         await update.message.reply_text(text="Стартовое меню", reply_markup=keyboard)
-
     return states.MAIN_MENU_STATE
 
 
-async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Регистрирует пользователя."""
-    await services.create_user(update.effective_user)
-    await update.callback_query.edit_message_text(text="Вы зарегистрированы")
-    return await start_menu(update, context)
-
-
-async def bind_the_channel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Кнопка для связывания канала и аккаунта пользователя."""
-    text = "Добавьте бот в ваш канал и перешлите любое сообщение из этого канала в чат бота"
+async def call_binding(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Вызывает функцию привязки канала и блокирует пересылку сообщений в каналы пользователя."""
+    text = "Добавьте бот в ваш канал и перешлите любое сообщение из этого канала в этот чат"
     context.user_data[constants.STOP_FORWARD] = True
     await update.callback_query.answer()
     await update.callback_query.edit_message_text(text=text)
-    return states.BINDING_CHANNEL
+    return states.BINDING
 
 
-async def type_channel_for_binding(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Связывает аккаунт пользователя и канал."""
-    channel = await base.channel_repository.get_channel(update.message.forward_from_chat.id)
-    if not channel:
-        return await start_menu(update, context)
-    user = await base.user_repository.get_user(update.effective_user.id)
-    bind = await base.bind_repository.get_bind(user.id, channel.id)
-    if not bind:
-        is_channel_admin = await services.check_user_is_admin(update, context.bot)
-        if is_channel_admin:
-            await services.create_bind(user.id, channel.id)
-            await update.message.reply_text(text=f"Вы привязали канал '{update.message.forward_from_chat.title}'")
-    return await start_menu(update, context)
+async def binding_user_with_channel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Связывает аккаунт пользователя и канал, из которого переслали сообщение."""
+    text = ""
+    channel_title = update.message.forward_from_chat.title
+
+    try:
+        channel = await base.channel_repository.get(update.message.forward_from_chat.id)
+        user = await base.user_repository.get(update.effective_user.id)
+        await services.user_is_admin_in_channel(update, context.bot)
+        await services.create_bind(user.id, channel.id)
+    except exceptions.ChannelNotFoundError:
+        text = f"Канал '{channel_title}' не найден в БД. Добавьте бот в канал еще раз"
+    except exceptions.ObjectAlreadyExistsError:
+        text = f"Канал '{channel_title}' уже привязан к вашему аккаунту"
+    except exceptions.UserNotAdminInChannelError:
+        text = f"Вы не являетесь администратором канала '{channel_title}'"
+    else:
+        text = f"Вы привязали канал '{channel_title}'"
+    finally:
+        await update.message.reply_text(text=text)
+        return await main_menu(update, context)
 
 
-async def my_channels(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Меню каналов."""
-    user = await base.user_repository.get_user(update.effective_user.id)
+async def user_channels(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Выводит меню с каналами, привязанными к данному аккаунту."""
+    user = await base.user_repository.get(update.effective_user.id)
     context.user_data[constants.CURRENT_USER] = user
     channels_buttons = []
     for bind in user.channels:
@@ -77,11 +77,11 @@ async def my_channels(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str
     keyboard = InlineKeyboardMarkup(buttons)
     await update.callback_query.answer()
     await update.callback_query.edit_message_text(text="Список привязанных каналов", reply_markup=keyboard)
-    return states.MY_CHANNELS_STATE
+    return states.USER_CHANNELS_STATE
 
 
 async def channel_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Меню канала."""
+    """Выводит меню выбранного канала."""
     buttons = [
         [InlineKeyboardButton("Изменить описание", callback_data=callback_data.CALLBACK_EDIT_DESCRIPTION)],
         [InlineKeyboardButton("Назад", callback_data=callback_data.CALLBACK_BACK_TO_CHANNELS)],
@@ -91,12 +91,10 @@ async def channel_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> st
     if update.callback_query:
         await update.callback_query.answer()
         await update.callback_query.edit_message_reply_markup(reply_markup=keyboard)
-        try:
+        if update.callback_query.data:
             channel_id = int(update.callback_query.data)
-            channel = await base.channel_repository.get_channel(channel_id)
+            channel = await base.channel_repository.get(channel_id)
             context.user_data[constants.CURRENT_CHANNEL] = channel
-        except ValueError:
-            pass
     else:
         await update.message.reply_text(text="Меню канала", reply_markup=keyboard)
     return states.CHANNEL_MENU_STATE
@@ -113,8 +111,9 @@ async def edit_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -
 
 async def input_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Обрабатывает введенное пользователем новое описание для канала."""
-    await services.change_bind_description(update, context.user_data)
-    await update.message.reply_text(text="Описание изменено")
+    new_description = update.message.text
+    await services.change_bind_description(new_description, context.user_data)
+    await update.message.reply_text(text=f"Описание изменено. Новое описание '{new_description}'")
     return await channel_menu(update, context)
 
 
