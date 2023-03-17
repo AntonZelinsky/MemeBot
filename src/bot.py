aaef3146d60df174cc992ae020b84bc888595ac1
pozarnik <pozarnik@users.noreply.github.com>
2023-03-17 23:45:12 +0300

    #10 remove START_MENU_STATE, renames

--- src/bot.py
+++ src/bot.py
@@ -23,35 +23,32 @@ def create_bot():
     builder.concurrent_updates(False)
     application = builder.build()
     menu_handler = ConversationHandler(
-        entry_points=[CommandHandler("menu", menu_commands.start_menu, filters.ChatType.PRIVATE)],
+        entry_points=[CommandHandler("menu", menu_commands.main_menu, filters.ChatType.PRIVATE)],
         states={
-            states.START_MENU_STATE: [
-                CallbackQueryHandler(menu_commands.start_menu, pattern=callback_data.CALLBACK_START_MENU),
-            ],
             states.MAIN_MENU_STATE: [
-                CallbackQueryHandler(menu_commands.register, pattern=callback_data.CALLBACK_REGISTER),
-                CallbackQueryHandler(menu_commands.bind_the_channel, pattern=callback_data.CALLBACK_BIND_THE_CHANNEL),
-                CallbackQueryHandler(menu_commands.my_channels, pattern=callback_data.CALLBACK_MY_CHANNELS),
+                CallbackQueryHandler(menu_commands.call_binding, pattern=callback_data.CALLBACK_CALL_BINDING),
+                CallbackQueryHandler(menu_commands.user_channels, pattern=callback_data.CALLBACK_USER_CHANNELS),
                 CallbackQueryHandler(menu_commands.close_menu, pattern=callback_data.CALLBACK_CLOSE_MENU),
             ],
-            states.MY_CHANNELS_STATE: [
+            states.USER_CHANNELS_STATE: [
                 CallbackQueryHandler(menu_commands.channel_menu, pattern=callback_data.CALLBACK_CHANNEL_MENU),
-                CallbackQueryHandler(menu_commands.start_menu, pattern=callback_data.CALLBACK_BACK_TO_MAIN),
+                CallbackQueryHandler(menu_commands.main_menu, pattern=callback_data.CALLBACK_BACK_TO_MAIN),
             ],
             states.CHANNEL_MENU_STATE: [
                 CallbackQueryHandler(menu_commands.edit_description, pattern=callback_data.CALLBACK_EDIT_DESCRIPTION),
-                CallbackQueryHandler(menu_commands.my_channels, pattern=callback_data.CALLBACK_BACK_TO_CHANNELS),
+                CallbackQueryHandler(menu_commands.user_channels, pattern=callback_data.CALLBACK_BACK_TO_CHANNELS),
             ],
-            states.BINDING_CHANNEL: [
-                MessageHandler(filters.ALL, menu_commands.type_channel_for_binding),
+            states.BINDING: [
+                MessageHandler(filters.ALL, menu_commands.binding_user_with_channel),
             ],
             states.TYPING_DESCRIPTION: [
                 MessageHandler(filters.TEXT, menu_commands.input_description),
             ],
         },
-        fallbacks=[CommandHandler("menu", menu_commands.start_menu)],
+        fallbacks=[CommandHandler("menu", menu_commands.main_menu)],
     )
-    application.add_handler(CommandHandler("start", handlers.start, filters.ChatType.PRIVATE))
+    application.add_handler(CommandHandler("start", handlers.start_handler, filters.ChatType.PRIVATE))
+    application.add_handler(CommandHandler("register", handlers.register_handler, filters.ChatType.PRIVATE))
     application.add_handler(menu_handler)
     application.add_handler(ChatMemberHandler(handlers.channel_chat_handler, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(
