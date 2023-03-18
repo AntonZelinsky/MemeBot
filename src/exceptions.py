09c89356e607777bf4fda5237597cac06b77647d
pozarnik <pozarnik@users.noreply.github.com>
2023-03-18 15:16:46 +0300

    #10 rename exceptions, edit error message

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,22 +1,22 @@
-class UserNotFoundError(Exception):
+class UserNotFound(Exception):
     def __init__(self, account_id: int) -> None:
-        message = f"Произошла ошибка при получении пользователя с account_id={account_id} из БД."
+        message = f"Пользователь с account_id={account_id} не найдет в БД."
         super().__init__(message)
 
 
-class ChannelNotFoundError(Exception):
+class ChannelNotFound(Exception):
     def __init__(self, channel_id: int) -> None:
-        message = f"Произошла ошибка при получении канала с channel_id={channel_id} из БД."
+        message = f"Канал с channel_id={channel_id} не найден в БД."
         super().__init__(message)
 
 
-class ObjectAlreadyExistsError(Exception):
-    def __init__(self, new_data: dict) -> None:
-        message = f"Произошла ошибка при создании объекта {new_data} в БД, объект с таким id уже существует."
+class ObjectAlreadyExists(Exception):
+    def __init__(self, instance) -> None:
+        message = f"Произошла ошибка при создании объекта {instance}. Объект с таким id уже существует в БД."
         super().__init__(message)
 
 
-class UserNotAdminInChannelError(Exception):
+class UserNotAdminInChannel(Exception):
     def __init__(self, account_id: int, channel_id: int) -> None:
-        message = f"Пользователь с account_id={account_id} не является администратором канала с channel_id={channel_id}"
+        message = f"Пользователь account_id={account_id} не является администратором канала channel_id={channel_id}"
         super().__init__(message)
