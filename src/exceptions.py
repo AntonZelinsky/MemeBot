fe7888df95136d5b77d16a6b6213de2ebea92b3e
pozarnik <pozarnik@users.noreply.github.com>
2023-03-19 01:21:42 +0300

    #10 fix call message

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,22 +1,25 @@
 class UserNotFound(Exception):
     def __init__(self, account_id: int) -> None:
-        message = f"Пользователь с account_id={account_id} не найдет в БД."
-        super().__init__(message)
+        self.account_id = account_id
+        super().__init__(f"Пользователь с account_id={self.account_id} не найдет в БД.")
 
 
 class ChannelNotFound(Exception):
     def __init__(self, channel_id: int) -> None:
-        message = f"Канал с channel_id={channel_id} не найден в БД."
-        super().__init__(message)
+        self.channel_id = channel_id
+        super().__init__(f"Канал с channel_id={self.channel_id} не найден в БД.")
 
 
 class ObjectAlreadyExists(Exception):
     def __init__(self, instance) -> None:
-        message = f"Произошла ошибка при создании объекта {instance}. Объект с таким id уже существует в БД."
-        super().__init__(message)
+        self.instance = instance
+        super().__init__(f"Ошибка при создании объекта {self.instance}. Объект с таким id уже существует в БД.")
 
 
 class UserNotAdminInChannel(Exception):
     def __init__(self, account_id: int, channel_id: int) -> None:
-        message = f"Пользователь account_id={account_id} не является администратором канала channel_id={channel_id}"
-        super().__init__(message)
+        self.account = account_id
+        self.channel = channel_id
+        super().__init__(
+            f"Аккаунт account_id={self.account_id} не является администратором канала channel_id={self.channel_id}",
+        )
