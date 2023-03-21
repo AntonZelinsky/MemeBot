b1a1158521de0b62767c7dbb7b08280f5edc31ca
pozarnik <pozarnik@users.noreply.github.com>
2023-03-22 02:35:54 +0300

    #10 update exceptions name, added BaseProjectException

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,22 +1,26 @@
-class UserNotFound(Exception):
+class BaseProjectException(Exception):
+    pass
+
+
+class UserNotFoundError(BaseProjectException):
     def __init__(self, account_id: int) -> None:
         self.account_id = account_id
         super().__init__(f"Пользователь с account_id={self.account_id} не найдет в БД.")
 
 
-class ChannelNotFound(Exception):
+class ChannelNotFoundError(BaseProjectException):
     def __init__(self, channel_id: int) -> None:
         self.channel_id = channel_id
         super().__init__(f"Канал с channel_id={self.channel_id} не найден в БД.")
 
 
-class ObjectAlreadyExists(Exception):
+class ObjectAlreadyExistsError(BaseProjectException):
     def __init__(self, instance) -> None:
         self.instance = instance
         super().__init__(f"Ошибка при создании объекта {self.instance}. Объект с таким id уже существует в БД.")
 
 
-class UserIsNotAdminInChannel(Exception):
+class UserIsNotAdminInChannel(BaseProjectException):
     def __init__(self, account_id: int, channel_id: int) -> None:
         self.account = account_id
         self.channel = channel_id
@@ -25,7 +29,7 @@ class UserIsNotAdminInChannel(Exception):
         )
 
 
-class BotKickedFromTheChannel(Exception):
+class BotKickedFromTheChannel(BaseProjectException):
     def __init__(self, channel_id: int) -> None:
         self.channel_id = channel_id
         super().__init__(f"Бот был удален из канала channel_id={channel_id}")
